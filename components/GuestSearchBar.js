e824ba403fa7716dd3ed592800d06b19ed73aa31
Olawale Otubu <waleoxy@gmail.com>
2022-01-03 02:20:54 +0100

    setup of the geafer section, nacbar and the css

---
+++ components/GuestSearchBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function GuestSearchBar() {
+  return (
+    <div className="absolute bg-white rounded-[10px] flex w-[1182px] h-[80px] left-[146px] top-[299px]"></div>
+  );
+}
+
+export default GuestSearchBar;
